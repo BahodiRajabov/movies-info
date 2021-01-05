4a27605c243decb26a7f071debb0c86c0c9e660e
Bahodir Rajabov <rbcoder12345@gmail.com>
2021-01-05 10:36:34 +0500

    Add header section and search form

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// set megin top on tablet and mobile mode
+document.body.style.marginTop =
+  document.querySelector(".header").offsetHeight + "px";
+
+// const newKinolar = kinolar.map((kino) => {
+//   kino.rating = Number((Math.random() * 10).toFixed(1));
+//   return kino;
+// });
+// console.log(JSON.stringify(newKinolar));
